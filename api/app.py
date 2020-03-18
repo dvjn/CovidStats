99125ebfc20bcf1235c16335b15e63d039357ef8
divykjain <divykjain@gmail.com>
2020-03-18 12:43:18 +0530

    completed api v0.2

--- api/app.py
+++ api/app.py
@@ -1,32 +1,24 @@
 from flask import Blueprint, request
 
+from numpy import nan
 import json
-from .data import covid_data, locations, params
+from .data import covid_data, countries, states, params
 
 api = Blueprint('api', __name__)
 
-def select_by_country_state(country, state):
-    if country and state:
-        return covid_data[(covid_data['Country/Region']==country) & (covid_data['Province/State']==state)]    
-    elif country:
-        return covid_data[covid_data['Country/Region']==country]
-    else:
-        return covid_data
-
 @api.route('/get_time_series/', methods=['GET'])
 def time_series_data():
-    data = select_by_country_state(request.args.get('country'), request.args.get('state'))
-    data = data.groupby(by=['Date']).sum()[params]
-    return data.to_dict()
-
-@api.route('/get_locations/', methods=['GET'])
-def get_locations():
-    return locations.to_dict("index")
+    country, state = request.args.get('country', 'All'), request.args.get('state', 'All')
+    return {
+            'country': country, 
+            'state': state, 
+            'data': covid_data.loc[(country, state, slice(None)), :].reset_index(level='Country/Region', drop=True).reset_index(level='Province/State', drop=True).to_dict("index")
+        }
 
 @api.route('/get_countries/', methods=['GET'])
 def get_countries():
-    return locations['Country/Region'].unique().tolist()
+    return countries
 
-@api.route('/get_regions/', methods=['GET'])
-def get_regions():
-    return [region for region in locations[locations['Country/Region']==request.args.get('country')]['Province/State'].unique().tolist() if region!='NaN']
+@api.route('/get_states/', methods=['GET'])
+def get_states():
+    return states.get(request.args.get('country', 'All'), [])
--- api/data.py
+++ api/data.py
@@ -1,27 +1,43 @@
 import pandas as pd
+from collections import defaultdict
 
 params = ['Confirmed', 'Deaths', 'Recovered']
 
 data_link = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-{param}.csv'
 
-locations = None
-
 def get_data(param):
+    global locations
     link = data_link.format(param=param)
-    if param==params[0]:
-        global locations
-        data = pd.read_csv(link)
-        locations = data[['Country/Region', 'Province/State', 'Lat', 'Long']]
-        data = data.drop(['Lat', 'Long'], axis=1)
-    else:
-        data = pd.read_csv(link).drop(['Lat', 'Long'], axis=1)
-    data['Province/State'] = data['Province/State'].fillna('NaN')
-    return data.set_index(['Country/Region', 'Province/State']).stack()
+    data = pd.read_csv(link)
+    data['Province/State'] = data['Province/State'].fillna('All')
+    data = data[~data['Province/State'].str.contains(', ')]
+    data['Parameter'] = param
+    data = data.drop(['Lat', 'Long'], axis=1)
+    return data
 
-covid_data = pd.DataFrame({param:get_data(param) for param in params})
-covid_data.index = covid_data.index.rename(['Country/Region', 'Province/State', 'Date'])
-covid_data = covid_data.reset_index()
-# covid_data['Date'] = pd.to_datetime(covid_data['Date'])
+covid_data = pd.concat([get_data(param) for param in params], ignore_index=True).set_index(['Country/Region', 'Province/State', 'Parameter']).sort_index()
 
-if __name__=='__main__':
-    print(covid_data.info())
+country_aggregates = covid_data.groupby(level=[0,2]).sum()
+country_aggregates = pd.concat([country_aggregates], keys=['All'], names=['Province/State']).reorder_levels(['Country/Region', 'Province/State', 'Parameter'])
+
+global_aggregate = covid_data.groupby(level=[2]).sum()
+global_aggregate = pd.concat([global_aggregate], keys=['All'], names=['Province/State'])
+global_aggregate = pd.concat([global_aggregate], keys=['All'], names=['Country/Region'])
+
+covid_data = pd.concat([covid_data, country_aggregates, global_aggregate]).reset_index().drop_duplicates(subset=['Country/Region', 'Province/State', 'Parameter'], keep='first').set_index(['Country/Region', 'Province/State', 'Parameter']).sort_index()
+
+countries = set()
+states = defaultdict(list)
+
+for idx, _ in covid_data.groupby(level=[0,1]):
+    countries.add(idx[0])
+    states[idx[0]].append(idx[1])
+
+def sort_with_all(data):
+    data = sorted(data)
+    data.insert(0, data.pop(data.index('All')))
+    return data
+
+countries = sort_with_all(countries)
+for country in states.keys():
+    states[country] = sort_with_all(states[country])
